bc690f6796ccc78501080ce91aa428ff0517f25d
Chris <chrisjahim@free.fr>
2017-11-09 10:28:25 +0100

    add: Ajout classe Document

---
+++ src/projetdocnumerique/Document.java
@@ -0,0 +1,14 @@
+/*
+* Projet de Documents Numérique
+* Smail KHAMED, Clément COLIN, Dimitri BRUYERE, Christopher JEAMME
+ */
+package projetdocnumerique;
+
+/**
+ *
+ * @author Chris
+ */
+public class Document
+{
+    
+}
