5a78a2d534338d27af2a3cc6c3c5a173c2da2977
Chris <chrisjahim@free.fr>
2017-11-09 11:52:15 +0100

    feat: Avancement du parseur

--- src/projetdocnumerique/ProjetDocNumerique.java
+++ src/projetdocnumerique/ProjetDocNumerique.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* Projet de Documents Numérique
+* Smail KHAMED, Clément COLIN, Dimitri BRUYERE, Christopher JEAMME
+*/
 package projetdocnumerique;
 
 /**
@@ -17,7 +16,9 @@ public class ProjetDocNumerique
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        //Parser
+        //Vérifier
+        //Traiter
     }
     
 }
