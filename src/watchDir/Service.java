59e370a6f05cfda744aacf005ed31eb355b0e86b
Chris <chrisjahim@free.fr>
2017-12-03 06:59:27 +0100

    feat: add library WatchDir

---
+++ src/watchDir/Service.java
@@ -0,0 +1,20 @@
+package watchDir;
+
+/**
+ * Interface definition for services.
+ */
+public interface Service
+{
+
+    /**
+     * Starts the service. This method blocks until the service has completely
+     * started.
+     */
+    void start() throws Exception;
+
+    /**
+     * Stops the service. This method blocks until the service has completely
+     * shut down.
+     */
+    void stop();
+}
