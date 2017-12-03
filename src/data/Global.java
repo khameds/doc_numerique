14c33bb87d75a3723aa2f6990a1081f839f59c90
KHAMED Smaïl <khameds@gmail.com>
2017-12-03 18:22:56 +0100

    [Feat] Global class : Add several path constants (xsd file, database, output trace file).

--- src/data/Global.java
+++ src/data/Global.java
@@ -1,6 +1,8 @@
 package data;
 
 public class Global {
-    public static final String INPUT_XML_FILE_PATH = "fichiers_test/valides/minimalist_file.xml";
+    public static final String XSD_FILE_PATH = "fichiers_definitions/definition.xsd";
+    public static final String INPUT_XML_FILE_PATH = "fichiers_test/syntaxe/non_valides/multiple_msg_collection.xml";
     public static final String OUTPUT_TRACE_FILE_PATH = "";
+    public static final String DB_PATH = "database.db";
 }
