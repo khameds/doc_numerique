40274d48e35b192f0f06345aecb76e75d1d365cc
KHAMED Smaïl <khameds@gmail.com>
2017-12-03 18:23:38 +0100

    [Feat] Database class : Implement connection, executeQuery, close functions.

--- src/data/Database.java
+++ src/data/Database.java
@@ -1,13 +1,51 @@
-/*
-* Projet de Documents Numérique
-* Smail KHAMED, Clément COLIN, Dimitri BRUYERE, Christopher JEAMME
- */
 package data;
 
-/**
- *
- */
-public class Database
-{
-    
+import java.sql.*;
+
+public class Database {
+    private String DBPath;
+    private Connection connection = null;
+    private Statement statement = null;
+
+    public Database(String dBPath) {
+        DBPath = dBPath;
+    }
+
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DBPath);
+            statement = connection.createStatement();
+            System.out.println("Successfully connected to " + DBPath + " !");
+        } catch (ClassNotFoundException notFoundException) {
+            notFoundException.printStackTrace();
+            System.out.println("Error : Database connection failed (ClassNotFoundException)...");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.out.println("Error : Database connection failed (SQLException)...");
+        }
+    }
+
+    public ResultSet executeQuery(String query){
+        ResultSet rs = null;
+        try{
+            rs = statement.executeQuery(query);
+        }
+        catch (SQLException e) {
+            System.out.println("Error executeQuery(" + query +")");
+            e.printStackTrace();
+        }
+
+        return rs;
+    }
+
+    public void close() {
+        try {
+            connection.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.out.println("Error while closing database connection");
+            e.printStackTrace();
+        }
+    }
 }
