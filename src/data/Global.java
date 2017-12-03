cd1011994cf4c37a79f56c4561b37a9ac2a1f024
KHAMED Smaïl <khameds@gmail.com>
2017-12-03 18:30:18 +0100

    [Feat] Global class : Add FILE_RECEIVING_FOLDER  path.

--- src/data/Global.java
+++ src/data/Global.java
@@ -5,4 +5,5 @@ public class Global {
     public static final String INPUT_XML_FILE_PATH = "fichiers_test/syntaxe/non_valides/multiple_msg_collection.xml";
     public static final String OUTPUT_TRACE_FILE_PATH = "";
     public static final String DB_PATH = "database.db";
+    public static final String FILE_RECEIVING_FOLDER = "FileReceivingFolder";
 }
