18c8eb455f37b52e0b469edc3276d377af304032
Chris <chrisjahim@free.fr>
2017-12-06 03:23:07 +0100

    feat: après le traitement d'un xml, la sortie est maintenant écrite dans un fichier (Dossier OutputFolder)

--- src/server/Server.java
+++ src/server/Server.java
@@ -13,7 +13,13 @@ import java.io.IOException;
 import data.Global;
 import data.Message;
 import data.TypeMessage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import parser.HandlerSAX;
 import parser.ParserSAX;
 import parser.Validating;
@@ -93,7 +99,7 @@ public class Server
      * @param pathToXMLFile Path to the XML file
      */
     public void startComputing(String pathToXMLFile)
-    {
+    {        
         //XSD utilisé
         String pathToXSD = Global.XSD_FILE_PATH;
         String outputXMLFile = Global.OUTPUT_TRACE_FILE_PATH;
@@ -226,7 +232,11 @@ public class Server
                     break;
             }
         }
-        return "";
+        
+        // EXEMPLE DE LOG
+        // Logger.getLogger(Server.class.getName()).log(Level.INFO,"SALUT");
+        
+        return "(XML DE SORTIE)";
     }
 
     /**
@@ -235,7 +245,21 @@ public class Server
      */
     private void writeResponse(String outputXMLFile)
     {
-        System.out.println("OUTPUT = " + outputXMLFile); //TEMPORAIRE
+        System.out.println("OUTPUT XML = " + outputXMLFile); //TEMPORAIRE
         //Il faudra écrire ça dans un fichier de sortie
+        
+        try
+        {
+            FileOutputStream fos = new FileOutputStream(new File("OutputFolder/"+new Date().toString().replace(":", "-")+".xml")); //Le nom du fichier de sortie est la date
+            fos.write(outputXMLFile.getBytes());
+        }
+        catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
